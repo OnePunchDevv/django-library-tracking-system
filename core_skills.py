46e4a64478f37738d93358241dbbc3bf8f07689c
OnePunchDevv <musakallemm@gmail.com>
2025-03-24 12:02:43 -0700

    basic python tasks

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,14 @@
 import random
 # rand_list =
 
-# list_comprehension_below_10 =
+def rand_list():
+    rand_nums = [random.randint(1, 20) for _ in range(10)]
+    return rand_nums
 
 # list_comprehension_below_10 =
+def list_comprehension_below_10(rand_nums):
+    return [ num for num in rand_nums if num < 10]
+
+# list_comprehension_below_10 =
+def list_comprehension_below_10(rand_nums):
+    return list(filter(lambda num: num < 10, rand_nums))
